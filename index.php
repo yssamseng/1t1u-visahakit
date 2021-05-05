6df2e9a9e3138e04e50b8af23898d85ed90200ce
yssamseng <ys.samseng@gmail.com>
2021-05-05 15:56:48 +0700

    make it better

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+    require_once('api/dbconfig.php');
+
+
+    $db = new Database('localhost','1t1u','root','');
+
+    if ($_SERVER['REQUEST_METHOD'] == "GET"){
+        echo json_encode($db->query('SELECT * FROM innovation'));
+    }else if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        echo 'This is post';
+    }else {
+        http_response_code(405);
+    }
 
 ?>
